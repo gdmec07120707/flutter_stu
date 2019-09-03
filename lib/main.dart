d120f05c76f6fb13193db8c23c405d50bec1fc09
fangdingjie <83678209@qq.com>
2019-09-03 15:29:03 +0800

    抽离ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'model/Post.dart';
+import 'demo/ListViewDemo.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,30 +26,7 @@ class MyHomePage extends StatelessWidget {
           title: Text("Flutter"),
           elevation: 0,
         ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _itemBuildView,
-        ));
-  }
-
-  Widget _itemBuildView(BuildContext context, int index) {
-    return Container(
-      child: new Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          SizedBox(height: 5),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16),
-        ],
-      ),
+        body: null
     );
   }
 }
